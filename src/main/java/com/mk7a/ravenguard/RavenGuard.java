99aa4688a1f7daf4814bd5a233357cc9cfe8b061
mk7a <git@mk7.dev>
2023-11-14 12:13:49 +0000

    init

---
+++ src/main/java/com/mk7a/ravenguard/RavenGuard.java
@@ -0,0 +1,13 @@
+package com.mk7a.ravenguard;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RavenGuard implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/com/mk7a/ravenguard/client/RavenGuardClient.java
@@ -0,0 +1,13 @@
+package com.mk7a.ravenguard.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RavenGuardClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
